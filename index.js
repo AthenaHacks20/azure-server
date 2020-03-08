1c8e9efbbb62f4a5cb3554af144a1d85e039d1ae
Nadya Djojosantoso <hello@nadyafebi.com>
2020-03-07 18:43:26 -0800

    Add websocket and logger

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
 const http = require('http');
 const express = require('express');
+const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 3000;
 
-const db = {};
+let logs = '';
+const db = {
+    sockets: {}
+};
+
+const log = (...msg) => {
+    let newLog = '<p>';
+    for (m of msg) {
+        if (typeof m === 'object') {
+            newLog += JSON.stringify(m) + ' ';
+        } else {
+            newLog += m.toString() + ' ';
+        }
+    }
+    newLog += '</p>';
+    logs += newLog;
+    console.log(...msg);
+};
 
 const app = express();
 const server = http.Server(app);
+const wss = new WebSocket.Server({ server });
 server.listen(PORT, () => {
-    console.log(`Server running in port ${PORT}!`);
+    log(`Server running on port ${PORT}!`);
 });
 
 app.get('/', (req, res) => {
+    res.send(logs);
+});
+
+app.get('/db', (req, res) => {
     res.send(db);
+});
+
+wss.on('connection', (ws) => {
+    const id = Math.random().toString(36).substr(2, 9);
+    db.sockets[id] = ws;
+    log(`${id}: Connected!`);
+
+    ws.on('message', (data) => {
+        try {
+            data = JSON.parse(data.toString());
+            log(`${id}:`, data);
+        } catch {
+            log(`${id}: Data parse error!`)
+        }
+    });
+
+    ws.on('close', () => {
+        delete db.sockets[id];
+        log(`${id}: Disconnected!`);
+    });
 });
